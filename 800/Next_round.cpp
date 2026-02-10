3a897f3be220481b314cb252f3e6cf7f3c67857c
Pranav kumar reddy k <kondapurampranav@gmail.com>
2026-02-10 22:57:10 +0530

    Added Next round

---
+++ 800/Next_round.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+ 
+int main(){
+    int n,k,x,count=0;
+    cin >> n >> k;
+    int arr[n];
+    
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+        
+    }
+    for(int i=0;i<n;i++){
+        if(arr[i] == 0) continue;
+        else if(arr[i] >= arr[k-1]){
+            count++;
+        }
+        
+    }
+    
+    cout << count;
+    return 0;
+}
